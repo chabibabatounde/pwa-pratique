cc9e7fa013edab32d96e171e05c318a0311b629e
Rodolpho BABATOUNDE <chabibabatounde@gmail.com>
2019-12-03 21:45:47 +0100

    Service Worker - résultat

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ console.log('hello depuis main');
 const technosDiv = document.querySelector('#technos');
 
 function loadTechnologies(technos) {
+    
     fetch('http://localhost:3001/technos')
         .then(response => {
             response.json()
                 .then(technos => {
                     const allTechnos = technos.map(t => `<div><b>${t.name}</b> ${t.description}  <a href="${t.url}">site de ${t.name}</a> </div>`)
                             .join('');
-            
                     technosDiv.innerHTML = allTechnos; 
                 });
         })
@@ -16,3 +16,11 @@ function loadTechnologies(technos) {
 }
 
 loadTechnologies(technos);
+
+
+// ..
+if(navigator.serviceWorker) {
+    navigator.serviceWorker
+        .register('sw.js')
+        .catch(err => console.error('service worker NON enregistré', err));
+}
